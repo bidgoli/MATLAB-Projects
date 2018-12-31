7dd458c1fcd2d562105b36331d2f56e238c940a1
bidgoli <45851987+bidgoli@users.noreply.github.com>
2018-12-31 13:36:42 +0330

    Update lin_eq_slov.m

--- Files/lin_eq_slov.m
+++ Files/lin_eq_slov.m
@@ -1,7 +1,22 @@
-function s=lin_eq_solv(N,k,first)
+function s=lin_eq_solv(N,k,restrictions,first)
+
+    % returning answers of the equation x_1+x_2+...+x_N=k
+    % r=restrictions is supposed to be a 1*N vector
+    % with restrictions x_1<=r_1 , x_2<=r_2 , ... , x_N<=r_N
+    % github.com/bidgoli
+    
+    
     if nargin==2
         first=1;
+        restrictions=N*ones(1,N);
     end
+    if nargin==3
+        first=1;
+    end
+    if first==1
+        restrictions=restrictions(N:-1:1);
+    end
+    
     if N<0
         disp('N must be non-negative!')
         return
@@ -9,7 +24,7 @@ function s=lin_eq_solv(N,k,first)
     s=[];
     
     if first==N
-        s=[k];
+        s=[min(restrictions(N),k)];
         return 
     end
     
@@ -17,13 +32,13 @@ function s=lin_eq_solv(N,k,first)
        s=zeros(1,N-first+1);
        return
     end
-        
-    for i=0:k
-       temp=lin_eq_solv(N,k-i,first+1);
+     
+    for i=max(0,k-sum(restrictions(first):N)):min(k,restrictions(first))
+       temp=lin_eq_solv(N,k-i,restrictions,first+1);
        temp(:,size(temp,2)+1)=i;
        s=[s;temp];
     end
         
         
-        
+     
 end
